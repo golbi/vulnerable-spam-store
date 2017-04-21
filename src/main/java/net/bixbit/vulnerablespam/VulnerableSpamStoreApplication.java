c0dcea0dcfaf24f233cd57a0aba5fb152bd55967
Krzysztof Benedyczak <golbi@icm.edu.pl>
2017-04-21 23:52:46 +0200

    skeleton

---
+++ src/main/java/net/bixbit/vulnerablespam/VulnerableSpamStoreApplication.java
@@ -0,0 +1,14 @@
+package net.bixbit.vulnerablespam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerableSpamStoreApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(VulnerableSpamStoreApplication.class, args);
+	}
+}
